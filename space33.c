73f35f36f5a71f6dd07f20ca6f564421f531a451
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-09 15:57:48 +0530

    space
    code

---
+++ space33.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch[100],i,b,sum=0;
+printf("\n enter a string:");
+gets(ch);
+b=strlen(ch);
+for(i=0;i<b;i++)
+{
+if(a[i]==' ')
+sum=sum=i;
+}
+printf("\n the no of spaces is %d",sum);
+getch();
+}
