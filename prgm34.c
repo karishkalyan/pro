2fd28c33e7838551290af91385211cc31189b920
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-09 16:00:28 +0530

    prgm
    code

---
+++ prgm34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch[100],i,b,sum=0,c;
+printf("\n enter a string:");
+gets(ch);
+b=strlen(ch);
+for(i=0;i<b;i++)
+{
+if(a[i]=='.')
+sum=sum+1;
+}
+c=sum+1
+printf("\n the no of spaces is %d",sum);
+return0;
+}
