ff94f2671a3a09db886618492f9b0ee3e17d83ec
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-09 16:06:46 +0530

    prgm
    code

---
+++ numeric35.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch[100],i,b,sum=0;
+printf("\n enter a string:");
+gets(ch);
+b=strlen(ch);
+for(i=0;i<b;i++)
+{
+if(0<=a[i])
+sum=sum+1;
+}
+printf("\n the no of numerics is %d",sum);
+getch();
